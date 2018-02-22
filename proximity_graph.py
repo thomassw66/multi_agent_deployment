1d5fdc2634d15367fe3e6be1a1f74aa43854c856
thomas wheeler <thomassw66@gmail.com>
2018-02-22 12:46:55 -0700

    basic gradient funcitons

--- proximity_graph.py
+++ proximity_graph.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-# takes a one dimentional array of points [ [x0, y0], [x1, y1], ...] and computes a square distance matrix to each point
+# takes a one dimentional array of points [ [x0, y0], [x1, y1], ...]
+# and computes a square distance matrix to each point
+#
 def make_proximity_graph(points):
 	x = points[:, 0]
 	y = points[:, 1]
@@ -12,6 +14,7 @@ def make_proximity_graph(points):
 	return np.sqrt ( np.square(dx) + np.square(dy) );
 
 # given a distance matrix return 1 if the distance is <= specified radius
+#
 def r_disk_graph(prox_graph, r):
     return np.less(prox_graph, r).astype(dtype=int) # this feels more sciency
 
@@ -23,41 +26,70 @@ def get_neighborhood(i, points, adjacentcy_matrix):
             a.append(points[j])
     return np.array(a)
 
+# we call this to calculate an agents new direction
+# all we should need is the points and its actual neighbors
+# points [x, y]
+def calculate_gradient(point, neighbors):
+    return ellipsoid_gradient(point, 1.0, 3.0)
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+############## COST FUNCTION GRADIENTS #############
+# Ciruclar cost function gradient
+def circle_gradient(point):
+    return [2*point[0], 2*point[1]]
+
+def ellipsoid_gradient(point, a, b):
+    return [2 * point[0] / a**2 , 2 * point[1] / b ** 2]
+
 
 # points = np.array([[1,1], [2,2], [3,3]])
 # pg = make_proximity_graph(points)
 # rd = r_disk_graph(pg, 1.5)
-
 # print rd
-
 # for i in range(len(points)):
 #   print i
 #    print get_neighborhood(i, points, rd)
-
-
 
 fig = plt.figure()
 
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
 
-# Set up a contour plot
+"""
+# Set up a contour plot of some function
 x = np.linspace(-5, 5, 20);
 y = np.linspace(-5, 5, 20);
 [X, Y] = np.meshgrid(x , y);
 Z = np.sqrt( np.square(X) + np.square(Y) )
 plt.contour(X, Y, Z)
-
-
+"""
 # some random animations
 # TODO: replace this with waypoint generation
 TIME_STEPS = 10
+STEP_SIZE = 0.5
+MAX_RADIUS = 2.0
+
 points = np.random.random([10, 2]) * 10 - 5
 y = [points]
 for i in range(1, TIME_STEPS):
-    x = y[i-1]
-    x = x + 1
-    print x
+    x = y[i-1].copy()
+    # ***** CALCULATE NEW POSITIONS HERE *****
+    prox = make_proximity_graph(x)
+    r_disk = r_disk_graph(prox, MAX_RADIUS)
+    # calculate cost function gradient
+    for i in range(len(x)):
+        # update x[i]
+        n = get_neighborhood(i, x, r_disk)
+        grad = calculate_gradient(x[i], n)
+        grad = normalize(grad)
+        # normalize gradient
+        x[i] = x[i] - STEP_SIZE * grad  # add the negative gradient to the point
     y.append(x);
 y = np.array(y)
 
