0aaeabbc017408365816f749e8d3c0fc19a40e6e
thomassw66 <thomassw66@gmail.com>
2018-03-01 14:45:31 -0700

    pesky pyc files

--- simulation.py
+++ simulation.py
@@ -18,7 +18,7 @@ def calculate_gradient(point, neighbors):
 # GENERATE DATA
 integration_resolution = 50
 NUM_ADVERSARIES = 0
-TIME_STEPS = 50
+TIME_STEPS = 60
 STEP_SIZE = 0.3
 MAX_RADIUS = 1
 L = 10
@@ -28,10 +28,10 @@ K = 500
 R = 1
 #important_center1 = (-3, 0.0)
 #important_center2 = (3, 3.0) / np.sqrt(2)
-important_centers = [ (-3, 0.0),(3,3) / np.sqrt(2),(0,-3)]
+important_centers = [ (-0, 3),(3,3) / np.sqrt(2),(4,0)]
 
-NUM_AGENTS = 15
-agent_center = (-0.0, 0)
+NUM_AGENTS = 40
+agent_center = (-4, -4)
 agent_radius = 0.2
 
 X_LIM = (-5, 5)
@@ -43,7 +43,7 @@ SR = lambda x: x * (np.arctan(L * x) / np.pi + 0.5)
 def ellipse(xc,yc,a,b,r):
 	return lambda X, Y: a*(X - xc)**2 + b*(Y - yc)**2 - r**2
 
-importance_function = lambda X, Y: sum([np.exp(-K * SR( ellipse(c[0],c[1],A,B,R)(X,Y) )) for c in important_centers])
+importance_function = lambda X, Y: sum( [ np.exp(-np.square(X-c[0])-np.square(Y-c[1])) for c in important_centers] )
 
 dist_square = lambda X, Y, center: np.square(X - center[0]) + np.square(Y-center[1])
 
