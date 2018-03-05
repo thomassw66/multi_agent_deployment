7b5bb2bdc4b2ae9b8411a6fcb9f4e09b92b5f77c
thomassw66 <thomassw66@gmail.com>
2018-03-05 12:24:31 -0700

    clean up some commented out code

--- simulation.py
+++ simulation.py
@@ -6,97 +6,95 @@ import contours
 from util import *
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-
-# we call this to calculate an agents new direction
-# all we should need is the points and its actual neighbors
-# points [x, y]
-def calculate_gradient(point, neighbors):
-    return 4 * grads.potential_field(point, neighbors) + \
-            1.0 * grads.ellipsoid(point,0.7,0.7, center=(4,3)) + \
-            1.0 * grads.ellipsoid(point,0.7,0.7, center=(-4,3))
-
-# GENERATE DATA
+# General Parameters 
 integration_resolution = 50
-NUM_ADVERSARIES = 0
-TIME_STEPS = 60
+NUM_ADVERSARIES = 6
+TIME_STEPS = 50
 STEP_SIZE = 0.3
-MAX_RADIUS = 1
+MAX_RADIUS = 2.9
 L = 10
 A = 0.5
 B = 0.5
 K = 500
 R = 1
-#important_center1 = (-3, 0.0)
-#important_center2 = (3, 3.0) / np.sqrt(2)
-important_centers = [ (-0, 3),(3,3) / np.sqrt(2),(4,0)]
 
-NUM_AGENTS = 40
-agent_center = (-4, -4)
+important_centers = [(-3, 0),(3,3) / np.sqrt(2)]
+
+NUM_AGENTS = 16
+agent_center = (0, 0)
 agent_radius = 0.2
 
-X_LIM = (-5, 5)
+# Boundaries of our environment 
+X_LIM = (-5, 5) 
 Y_LIM = (-5, 5)
 
-# smooth_ramp
-SR = lambda x: x * (np.arctan(L * x) / np.pi + 0.5)
-
-def ellipse(xc,yc,a,b,r):
-	return lambda X, Y: a*(X - xc)**2 + b*(Y - yc)**2 - r**2
-
+# we will define this to be a multi-modal gaussian centered at each of the centers in importance centers 
 importance_function = lambda X, Y: sum( [ np.exp(-np.square(X-c[0])-np.square(Y-c[1])) for c in important_centers] )
 
+# This is a helper for computing the gradient (grad H(P)) of our objective function 
 dist_square = lambda X, Y, center: np.square(X - center[0]) + np.square(Y-center[1])
 
-points = make_random_points(NUM_AGENTS, agent_radius, center=agent_center) # np.random.random([10, 2]) * 10 - 5
-angles = np.linspace(np.pi, np.pi*3/2, NUM_ADVERSARIES)
+points = make_random_points(NUM_AGENTS, agent_radius, center=agent_center) 
+# TODO: make this less ugly 
 points[NUM_AGENTS - NUM_ADVERSARIES: NUM_AGENTS] = [
-    [-1.5, -0.8], 
+    [-.5, .2], 
     [.0, 1.], 
-    [-.8, -1.5],
-    [-0.6, -0.6],
-    [-1.0, -0.2],
+    [.2, -.5],
+    [.4, .4],
+    [-.6, -0.6],
     [ 1.0, 0.0],
     ][:NUM_ADVERSARIES] + important_centers[1] if NUM_ADVERSARIES > 0 else np.zeros((0,2))
 
+# we store points and voronoi diagrams at each time step 
 y = [points]
 v = [Voronoi(points)]
+
+# run our algorithm for TIME_STEPS 
 for t in range(1, TIME_STEPS):
-    print "********** Time Step ", t, " **************"
+    
+    print "********** Time Step ", t, " / ", TIME_STEPS ," **************"
     x = y[t-1].copy()
+    vor_partition = v[-1]
+
     # ***** CALCULATE NEW POSITIONS HERE *****
-    #prox = make_proximity_graph(x)
-    #r_disk = r_disk_graph(prox, MAX_RADIUS)
-    # calculate cost function gradient
     for i in range(len(x)):
-        if i < NUM_AGENTS - NUM_ADVERSARIES: 
+        if i < NUM_AGENTS - NUM_ADVERSARIES:
+            # M = integr {Vi} f(|q - pi|) phi(q) dq
             mass = integrate_r_limited_voronoi(
                 lambda X, Y: dist_square(X,Y, x[i]) * importance_function(X, Y), 
-                x, i, x[i], MAX_RADIUS, v[-1], integration_resolution
+                x, i, x[i], MAX_RADIUS, vor_partition, integration_resolution
             )
+            # Mx = integr {Vi} xi * f(|q - pi|) phi(q) dq
             mx = integrate_r_limited_voronoi(
                 lambda X, Y: X * dist_square(X,Y,x[i]) * importance_function(X, Y),
-                x, i, x[i], MAX_RADIUS, v[-1], integration_resolution
+                x, i, x[i], MAX_RADIUS, vor_partition, integration_resolution
             )
+            # My = integr {Vi} yi * f(|q - pi|) phi(q) dq 
             my = integrate_r_limited_voronoi(
                 lambda X, Y: Y * dist_square(X,Y, x[i]) * importance_function(X, Y), 
-                x, i, x[i], MAX_RADIUS, v[-1], integration_resolution
+                x, i, x[i], MAX_RADIUS, vor_partition, integration_resolution
             )
-            print "i: ", i, mass, mx, my
             if mass > 0:
                 mx /= mass
                 my /= mass
             
-            grad = [mx, my] - x[i]  # moves in the direction of centriod
+            centroid = [mx, my]
+            
+            # moves in the direction of centriod
+            grad = centroid - x[i]  
             size = np.linalg.norm(grad)
-            # if size <= 1:
             grad = normalize(grad)
             alpha = min(STEP_SIZE * size, STEP_SIZE)
             # check if intersection with an obstacle ??
             x[i] = x[i] + alpha * grad
         else:
-            x[i] = x[i] # stay where you are 
+            # for this example the adversaries positions are static 
+            x[i] = x[i]  
+    
+    # save our calculations to display later 
     y.append(x)
     v.append(Voronoi(x))
+
 y = np.array(y)
 
 
@@ -112,7 +110,7 @@ ax.set_ylim(Y_LIM)
 # voronoi
 #voronoi, = ax.plot([], [], 'ro')
 voronoi_lin_segs = []
-for i in range(35):
+for i in range(NUM_AGENTS*4):
     vor_lin_seg, = ax.plot([], [], 'k-', lw=1)
     voronoi_lin_segs.append(vor_lin_seg)
 
@@ -139,14 +137,16 @@ def animate(i):
     a_point.set_data(y[i,NUM_AGENTS-NUM_ADVERSARIES:,0], y[i, NUM_AGENTS-NUM_ADVERSARIES:,1])
 
     # draw voronoi partitions
-    #vor = v[i]
-    #important_length = min(len(vor.ridge_vertices), len(voronoi_lin_segs))
-    #for i in range(important_length):
-    #	simplex = np.asarray(vor.ridge_vertices[i])
-    #	pointidx = vor.ridge_points[i] # is this always the same length?
-    # 	if np.all(simplex >= 0):
-    #		voronoi_lin_segs[i].set_data(vor.vertices[simplex, 0], vor.vertices[simplex, 1])
- 	#else:
+    vor = v[i]
+    important_length = min(len(vor.ridge_vertices), len(voronoi_lin_segs))
+    for i in range(important_length):
+    	simplex = np.asarray(vor.ridge_vertices[i])
+    	pointidx = vor.ridge_points[i] # is this always the same length?
+     	if np.all(simplex >= 0):
+    		voronoi_lin_segs[i].set_data(vor.vertices[simplex, 0], vor.vertices[simplex, 1])
+ 	else:
+            # CALCULATE VORONOI LINE SEGMENTS THAT ARE NOT FINITELY BOUNDED 
+            # THIS IS STILL REALLY BUGGY, LOOKS BETTER IF THESE ARE IGNORED FOR NOW
             #center = points.mean(axis=0)
             #e = simplex[simplex >= 0][0] # finite end voronoi vertex
             # i = simplex[simplex >= 0][0] # finite end Voronoi vertex
@@ -155,14 +155,14 @@ def animate(i):
             #n = np.array([-t[1], t[0]]) # normal
             #midpoint = points[pointidx].mean(axis=0)
             # the far point is going to be along this line intersecting with the boundary of the environment
-            #far_point = vor.vertices[e] + np.sign(np.dot(vor.vertices[e] - center, n)) * n * 100
+            #far_point = vor.vertices[e] + np.sign(np.dot(vor.vertices[e] - center, n)) * n * 10
             #voronoi_lin_segs[i].set_data([vor.vertices[e,0], far_point[0]],
             #      [vor.vertices[e,1], far_point[1]])
-	#	voronoi_lin_segs[i].set_data([], [])
+		 voronoi_lin_segs[i].set_data([], [])
 
 
-    #for i in range(important_length, len(voronoi_lin_segs)):
-    #3    voronoi_lin_segs[i].set_data([], [])
+    for i in range(important_length, len(voronoi_lin_segs)):
+        voronoi_lin_segs[i].set_data([], [])
 
 
     return point
